419e01937bf56f30d9004b3540c7c2e2295f3e49
Jason <jasondsouza212@gmail.com>
2022-04-24 21:07:10 +0530

    search is made more relaiable and efficient using filter and includes methods and toString

--- src/components/AllJokes.js
+++ src/components/AllJokes.js
@@ -6,7 +6,7 @@ export default function AllJokes() {
   const jokeobj=JSON.parse(localStorage.getItem('joke'))
   const [search,setSearchterm]=useState("")
   const [joke,setJoke]=useState(jokeobj)
-  const [currentJoke,setCurrentJoke]=useState(joke)
+  const [currentJoke,setCurrentJoke]=useState(jokeobj)
 
   //Is used for searching a term
   function searched(id){
@@ -17,21 +17,22 @@ export default function AllJokes() {
   function deleteres(id){
     setJoke(joke.filter(res=>res.id !== id))
     setCurrentJoke(joke)
+    localStorage.setItem('joke',JSON.stringify(joke));
   }
 
   //renders when the search is made
-  useEffect(()=>{
-    if(search==""){
-      setCurrentJoke(joke)
-    }else{
-      searched(search)
-    }
-  },[search])
+  // useEffect(()=>{
+  //   if(search==""){
+  //     setCurrentJoke(joke)
+  //   }else{
+  //     searched(search)
+  //   }
+  // },[search])
 
   //When ever a deletion is made the localstorage is updated
-  useEffect(()=>{
-    localStorage.setItem('joke',JSON.stringify(joke));
-  },[joke])
+  // useEffect(()=>{
+  //   localStorage.setItem('joke',JSON.stringify(joke));
+  // },[joke])
 
   return (
     <>
@@ -39,14 +40,20 @@ export default function AllJokes() {
   <h1>ALL JOKES</h1>
   <input placeholder='Search for Id Numbers'  onChange={(e)=>setSearchterm(e.target.value)} value={search} className='search' min={1} type="Number"></input>
   <ul class="ccards">
-     {currentJoke.map(item=>
+     {currentJoke.filter((val)=>{
+       if(search===" "){
+         return val
+       }else if(val.id.toString().toLowerCase().includes(search.toString().toLowerCase())){
+         return  val
+       }
+     }).map(val=>
           <li class="ccards_item">
            <div class="ccard">
              <div class="ccard_content">
-               <h2 class="ccard_title">Joke Number {item.id}</h2>
-               <p class="ccard_text">{item.Jokes}</p>
+               <h2 class="ccard_title">Joke Number {val.id}</h2>
+               <p class="ccard_text">{val.Jokes}</p>
                 <button
-                onClick={()=>deleteres(item.id)} 
+                onClick={()=>deleteres(val.id)} 
                 class="btn ccard_btn">DELETE</button> 
              </div>
            </div>
